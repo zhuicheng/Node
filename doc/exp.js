32e9f2d5363aa36a5bea4efeeb0448f64bec527c
馬驍 <wawl89757@126.com>
2014-11-20 16:47:44 +0800

    ...

---
+++ doc/exp.js
@@ -0,0 +1,15 @@
+// 在if(o) {} 的判断方法中，当o为0、false、null、undefined时不会进入方法体
+// Array对象具有map和forEach方法，map方法可以返回一个数组对象，而forEach不可以
+// Array对象的reduce方法用于将一个初始值和迭代参数对数组进行遍历：
+var arr = [ 1, 2, 3, 4 ];
+function itr(sum, o) {
+	return sum + o;
+}
+var sum = arr.reduce(itr, 0); // 10
+
+// Array对象的filter方法用于筛选符合条件的元素：
+var arr = [ 1, 2, 3, 4 ];
+function isEven(v) {
+	return v % 2 == 0;
+}
+var newArr = arr.filter(isEven); // [ 2, 4 ]
--- doc/index.js
+++ doc/index.js
@@ -12,17 +12,26 @@ function Content() {
 		return content;
 	};
 
-	appendContent('JavaScript：');
-	appendContent('在if(o) {} 的判断方法中，当o为0、false、null、undefined时不会进入方法体');
-	appendContent('Array对象具有map和forEach方法，map方法可以返回一个数组对象，而forEach不可以');
-	appendContent('NodeJs：');
-	appendContent('Chpt03 Loading Modules：');
-	appendContent('模块在首次加载后会被缓存起来，如果模块名能被解析为相同的文件名，则每次调用require均会返回同一模块（不要轻易修改引用模块的属性或方法，否则会影响到整个项目）');
+	appendContent('// 在if(o) {} 的判断方法中，当o为0、false、null、undefined时不会进入方法体');
+	appendContent('// Array对象具有map和forEach方法，map方法可以返回一个数组对象，而forEach不可以');
+	appendContent('// Array对象的reduce方法用于将一个初始值和迭代参数对数组进行遍历：');
+	appendContent('var arr = [ 1, 2, 3, 4 ];');
+	appendContent('function itr(sum, o) {');
+	appendContent('	return sum + o;');
+	appendContent('}');
+	appendContent('var sum = arr.reduce(itr, 0); // 10');
+	appendContent('');
+	appendContent('// Array对象的filter方法用于筛选符合条件的元素：');
+	appendContent('var arr = [ 1, 2, 3, 4 ];');
+	appendContent('function isEven(v) {');
+	appendContent('	return v % 2 == 0;');
+	appendContent('}');
+	appendContent('var newArr = arr.filter(isEven); // [ 2, 4 ]');
 }
 
 var fs = require('fs');
-fs.unlink('./exp.log', function() {
-	fs.open('./exp.log', 'a', function(e, fd) {
+fs.unlink('./exp.js', function() {
+	fs.open('./exp.js', 'a', function(e, fd) {
 		if (e) {
 			throw e;
 		}
